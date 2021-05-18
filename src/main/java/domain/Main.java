61cd10f1cc2b23fb97bc9c55351c461b82bb78f2
Mikhail9030 <gulaevmihail112@gmail.com>
2021-05-18 18:36:43 +0300

    Initial commit

---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Main {
+    public static double sum(final double[] a) {
+        double[] myArray = {3.,5.,7.,12.};
+        double sum=0.;
+        int index = 0;
+        while(index<myArray.length) {
+            sum+=myArray[i];
+            index++;
+        }
+        System.out.println(sum);
+    }
+}
